479befd8d8a183f16964715225a8e69958fe41aa
Robert Steed <robertsteed@Roberts-MacBook-Air.local>
2022-09-29 11:11:01 -0600

    segue lab finished

--- Segues/Segues/ViewController.swift
+++ Segues/Segues/ViewController.swift
@@ -12,16 +12,34 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.navigationItem.title = username.text
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsername {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = username.text
+        }
     }
     
 
     @IBOutlet weak var username: UITextField!
     
-    @IBAction func forgotPassword(_ sender: UIButton) { performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
-    }
+    @IBOutlet weak var forgotPassword: UIButton!
     
-    @IBAction func forgotUsername(_ sender: UIButton) { performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    @IBOutlet weak var forgotUsername: UIButton!
+    
+    @IBAction func forgotPassword(_ sender: UIButton) {
+        performSegue(withIdentifier: "errorPage", sender: sender)
+    
+        }
+    
+    
+    @IBAction func forgotUsername(_ sender: UIButton) {
+        performSegue(withIdentifier: "errorPage", sender: sender)
     }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- TallyApp/TallyApp/ViewController.swift
+++ TallyApp/TallyApp/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //PROPERTIES UP HERE
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var stepAmountField: UITextField!
+    
+    //LIFE CYCLE EVENTS AND INIT HERE
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //NORMAL FUNCTIONS HERE
+    
+    //IBACTIONS HERE
+    @IBAction func clearTotal(_ sender: UIButton) {
+        totalLabel.text = "0"
+    }
+    
+    
+    @IBAction func Add(_ sender: UIButton) {
+        var currentStepAmount = 1
+        
+        if let currentStepAmountText = stepAmountField.text, let currentStepAmountLocal = Int(currentStepAmountText) {
+            currentStepAmount = currentStepAmountLocal
+        }
+        
+        
+        
+        if let currentValueText = totalLabel.text, let currentValue = Int(currentValueText) {
+            totalLabel.text = "\(currentValue + currentStepAmount)"
+            
+        } else {
+            
+            totalLabel.text = "\(currentStepAmount)"
+        }
+    }
+    
+    @IBAction func subtract(_ sender: UIButton) {
+      guard let currentStepAmountText = stepAmountField.text, let currentStepAmount = Int(currentStepAmountText) else { return }
+        
+        if let currentValueText = totalLabel.text, let currentValue = Int(currentValueText) {
+            totalLabel.text = "\(currentValue - currentStepAmount)"
+            
+        } else {
+    
+            totalLabel.text = "\(currentStepAmount)"
+        }
+    }
 }
 
+//do a text box
+// set the add and subtract buttons to the input of the text box
+//or do a segmented control maybe at the bottom
+
+
+//
